2ef744506f5482503b4c1cb55ace02f20a614f0f
Gene Drabkin <gene.drabkin@gmail.com>
2013-04-24 22:10:33 -0700

    added benchmarks

--- README.md
+++ README.md
@@ -46,6 +46,29 @@ XmlHasher.parse("<tag1><tag2>content</tag2></tag1>")
 # => {:tag1=>{:tag2=>"content"}}
 
 ```
+## Benchmarks
+
+How fast is it?  Try it for your self - [benchmark.rb](https://github.com/cloocher/xmlhasher/blob/master/benchmark/benchmark.rb)
+
+```
+Benchmarking conversion of small xml from text to Hash
+                 user     system      total        real
+activesupport(rexml)     0.380000   0.000000   0.380000 (  0.385326)
+activesupport(libxml)    0.060000   0.000000   0.060000 (  0.062008)
+activesupport(nokogiri)  0.090000   0.000000   0.090000 (  0.089466)
+xmlsimple                0.480000   0.010000   0.490000 (  0.490938)
+nero                     0.120000   0.000000   0.120000 (  0.123612)
+xmlhasher                0.010000   0.000000   0.010000 (  0.017366)
+
+Benchmarking conversion of large xml from file to Hash
+            user     system      total        real
+activesupport(rexml)    57.230000   0.240000  57.470000 ( 57.460510)
+activesupport(libxml)   # Segmentation fault
+activesupport(nokogiri) 12.650000   0.250000  12.900000 ( 12.908073)
+xmlsimple               49.980000   0.160000  50.140000 ( 50.140775)
+nero                    15.590000   0.110000  15.700000 ( 15.697411)
+xmlhasher                4.290000   0.030000   4.320000 (  4.316379)
+```
 ## Requirements
 
 * Ruby 1.8.7 or higher
---
+++ banchmark/banchmark.rb
@@ -0,0 +1,95 @@
+$:.push File.expand_path('../../lib', __FILE__)
+
+require 'xmlhasher'
+require 'benchmark'
+begin
+  require 'nori'
+rescue
+  puts "nori gem in not install, run 'gem install nori'"
+end
+begin
+  require 'active_support/core_ext/hash/conversions'
+rescue
+  puts "active_support gem in not install, run 'gem install activesupport'"
+end
+begin
+  require 'xmlsimple'
+rescue
+  puts "xmlsimple gem in not install, run 'gem install xml-simple'"
+end
+begin
+  require 'nokogiri'
+rescue
+  puts "nokogiri gem in not install, run 'gem install nokogiri'"
+end
+begin
+  require 'libxml'
+rescue
+  puts "libxml gem in not install, run 'gem install libxml-ruby'"
+end
+
+
+runs = 100
+xml = File.read(File.expand_path('../../test/fixtures/institution.xml', __FILE__))
+puts 'Benchmarking conversion of small xml from text to Hash'
+Benchmark.bm 10 do |x|
+  ActiveSupport::XmlMini.backend = ActiveSupport::XmlMini_REXML
+  x.report 'activesupport(rexml)   ' do
+    runs.times { Hash.from_xml(xml) }
+  end
+
+  ActiveSupport::XmlMini.backend = 'LibXML'
+  x.report 'activesupport(libxml)  ' do
+    runs.times { Hash.from_xml(xml) }
+  end
+
+  ActiveSupport::XmlMini.backend = 'Nokogiri'
+  x.report 'activesupport(nokogiri)' do
+    runs.times { Hash.from_xml(xml) }
+  end
+
+  x.report 'xmlsimple              ' do
+    runs.times { XmlSimple.xml_in(xml) }
+  end
+
+  x.report 'nero                   ' do
+    runs.times { Nori.new(:advanced_typecasting => false).parse(xml) }
+  end
+
+  x.report 'xmlhasher              ' do
+    runs.times { XmlHasher.parse(xml) }
+  end
+end
+
+runs = 5
+puts
+path = File.expand_path('../../test/fixtures/institutions.xml', __FILE__)
+puts 'Benchmarking conversion of large xml from file to Hash'
+Benchmark.bm 5 do |x|
+  ActiveSupport::XmlMini.backend = ActiveSupport::XmlMini_REXML
+  x.report 'activesupport(rexml)   ' do
+    runs.times { Hash.from_xml(File.new(path)) }
+  end
+
+  ActiveSupport::XmlMini.backend = 'LibXML'
+  x.report 'activesupport(libxml)  ' do
+    #runs.times { Hash.from_xml(File.new(path)) } # Segmentation fault
+  end
+
+  ActiveSupport::XmlMini.backend = 'Nokogiri'
+  x.report 'activesupport(nokogiri)' do
+    runs.times { Hash.from_xml(File.new(path)) }
+  end
+
+  x.report 'xmlsimple              ' do
+    runs.times { XmlSimple.xml_in(path) }
+  end
+
+  x.report 'nero                   ' do
+    runs.times { Nori.new(:advanced_typecasting => false).parse(File.new(path).read) } # Nori doesn't support reading from a stream, load the file in memory
+  end
+
+  x.report 'xmlhasher              ' do
+    runs.times { XmlHasher.parse(File.new(path)) }
+  end
+end

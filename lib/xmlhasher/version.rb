7a7cf23fb3240294b57d61cb80f5138d0b5f24c9
dstepanenko <stepozer@gmail.com>
2019-12-05 22:06:23 +0300

    Use new ruby style guide and `rubocop` gem.

--- lib/xmlhasher/version.rb
+++ lib/xmlhasher/version.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 module XmlHasher
-  # rubocop:disable Style/RedundantFreeze, Lint/UnneededCopDisableDirective
-  # if frozen_string_literal is not supported
-  VERSION = '1.0.5'.freeze
-  # rubocop:enable Style/RedundantFreeze, Lint/UnneededCopDisableDirective
+  VERSION = '1.0.5'
 end
